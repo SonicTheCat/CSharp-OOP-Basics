27962e308c641f02eb2d30c5f7b3ba184014d3e5
SonicTheCat <paveliliev90@gmail.com>
2018-10-19 18:28:23 +0300

    INTERFACES AND ABSTRACTION LAB

---
+++ 09.Interfaces And Abstraction - Lab/02.02.Cars/Car.cs
@@ -0,0 +1,31 @@
+﻿public abstract class Car : ICar
+{
+    public string Model { get;}
+    public string Color { get; }
+
+    public Car(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color; 
+    }
+
+    public string Start()
+    {
+        return "Engine start"; 
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    protected virtual string GetCarInfo()
+    {
+        return $"{this.Color} {this.GetType().Name} {this.Model}"; 
+    }
+
+    public override string ToString()
+    {
+        return GetCarInfo() + "\n" + Start() + "\n" + Stop(); 
+    }
+}
---
+++ 09.Interfaces And Abstraction - Lab/02.02.Cars/Tesla.cs
@@ -0,0 +1,14 @@
+﻿public class Tesla : Car, IElectricCar
+{
+    public int Battery { get; }
+
+    public Tesla(string model, string color, int battery) : base(model, color)
+    {
+        this.Battery = battery; 
+    }
+
+    protected override string GetCarInfo()
+    {
+        return base.GetCarInfo() + $" with {this.Battery} Batteries"; 
+    }
+}
